575d8a32c57f57e2867079e26f4899a0fe0eae25
qinghua <hqh19990719@163.com>
2024-03-10 21:38:44 +0800

    pwm test demo and gpio test demo

---
+++ gpioTest/gpioTest.cpp
@@ -0,0 +1,13 @@
+#include <wiringPi.h>
+int main(void)
+{
+    wiringPiSetup() ;
+    pinMode (0, OUTPUT);
+    for(;;)
+    {
+        digitalWrite(0, HIGH); 
+        delay(500);
+        digitalWrite(0, LOW); 
+        delay(500);
+    }
+}
---
+++ pwmTest/pmwTest.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#include <softPwm.h>  
+int main( void)
+{
+    int pwm_gpio5 = 1; 
+    int i=0;
+    wiringPiSetup();   
+    pinMode(pwm_gpio5 ,PWM_OUTPUT);
+    printf("pwm_gpio5 is blinking...\n");    
+    for(;;)
+    {
+        for(i=0;i<1024;i++)
+        {
+            pwmWrite(1,i);
+            delay(10);
+            printf("Testing is %d......\n",i);    
+        }
+        for(i=1023;i>0;i--)
+        {
+            pwmWrite(1,i);
+            delay(10);
+            printf("Testing is %d......\n",i);    
+        }
+    }
+}
