df8c201bea147e7490aa2712b67efc26c2217f65
Qinghua Hao <hqh19990719@163.com>
2024-04-14 00:47:33 +0100

    update ui->text->append,due to \0,so we need transform array into string

--- TCP/clientwidget.cpp
+++ TCP/clientwidget.cpp
@@ -13,11 +13,13 @@ ClientWidget::ClientWidget(QWidget *parent)
     connect(tcpSocket,&QTcpSocket::connected,this,[=]()
     {
         ui->pushButtonConnect->setEnabled(false);
+        ui->textEdit->setText(QString("you have connected to our server"));
     });
     connect(tcpSocket,&QTcpSocket::readyRead,this,[=]()
     {
         QByteArray array = tcpSocket->readAll();
-        ui->textEdit->append(array);
+        QString hexString = array.toHex(' ');
+        ui->textEdit->append(hexString);
     });
     connect(ui->pushButtonOpen, &QPushButton::clicked, this, &ClientWidget::on_pushButtonOpen_clicked);
     connect(ui->pushButtonClose, &QPushButton::clicked, this, &ClientWidget::on_pushButtonClose_clicked);
